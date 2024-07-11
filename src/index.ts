e16839e2e91348e6b8a6d2cba21f4f866888094f
matje-p <138682310+matje-p@users.noreply.github.com>
2024-07-11 19:17:53 +0200

    removed console.log

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  console.log("hello world");
+  // console.log("hello world");
   res.send("Hello World");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  // console.log(`Server is running on port ${port}`);
 });
