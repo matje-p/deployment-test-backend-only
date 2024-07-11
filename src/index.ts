31c6ddf01d1a369a29d59755f2d83cb9810c8c8f
matje-p <138682310+matje-p@users.noreply.github.com>
2024-07-11 19:30:39 +0200

    some changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
+// src/index.ts
 import express, { Request, Response } from 'express';
 import { MongoClient } from 'mongodb';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// MongoDB connection string
 const uri = 'mongodb+srv://matje-p:ZNZGiQPvB0X86osf@cluster0.g4bap1z.mongodb.net/DeploymentTest?retryWrites=true&w=majority';
 
 // Function to connect to MongoDB and retrieve data
 async function getDataFromMongoDB() {
-  const client = new MongoClient(uri, {  });
+  const client = new MongoClient(uri, { });
 
   try {
     await client.connect();
@@ -28,15 +28,13 @@ async function getDataFromMongoDB() {
   }
 }
 
-app.get('/', async (req: Request, res: Response) => {
-  try {
-    const data = await getDataFromMongoDB();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).send('Error retrieving data');
-  }
+
+app.get('/', (req: Request, res: Response) => {
+  // console.log("hello world");
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  // console.log(`Server is running on port ${port}`);
 });
+
