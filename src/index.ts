55a7723498d599300ff2c140e4d51a4bceb94136
matje-p <138682310+matje-p@users.noreply.github.com>
2024-07-11 19:05:59 +0200

    stuff installed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-// index.js
-const express = require('express');
+// src/index.ts
+import express, { Request, Response } from 'express';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   console.log("hello world");
   res.send("Hello World");
 });
